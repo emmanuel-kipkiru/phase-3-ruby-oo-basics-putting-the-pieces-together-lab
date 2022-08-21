863ccfe85d84ed324636fe3896d1a0232e26fe87
emmanuel-kipkiru <emmanuelkipkirui17@gmail.com>
2022-08-22 00:38:36 +0300

    commit tree a2d65dc803d8d82d98520a5ed278108f5bfcde8a

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
+class Book
+    attr_reader  :title
+    attr_accessor  :author,:page_count,:genre
 
+
+    def initialize(title)
+        @title=title
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+book1 = Book.new("Eloquent Ruby")
+puts book1.title
+
+book1.author = "Manuel Diakins"
+puts book1.author
+
+book1.page_count = 800
+puts book1.page_count
+
+book1.genre = "Programming"
+puts book1.genre
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,32 @@
 # Make your shoe class here!
+class Shoe
+    attr_reader :brand
+    attr_accessor :color,:size,:material,:condition
+
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+
+
+end
+
+shoe1 = Shoe.new("Addidas")
+puts shoe1.brand
+
+shoe1.color = "white"
+puts shoe1.color
+
+shoe1.size = 43
+puts shoe1.size
+
+shoe1.material = "leather"
+puts shoe1.material
+
+shoe1.condition = "good"
+puts shoe1.material
